48e4dcd89ae07d4224dac6c2f87f93eb7bb347a6
princewebdev <coderboyprince@gmail.com>
2024-09-04 14:44:46 +0600

    Advanced Js Started

---
+++ advancedJSpartice/stringNumberDataType.js
@@ -0,0 +1,12 @@
+// print string with concat
+console.log("Hello Bangladesh");
+console.log("I Love Bangladesh" + "Nice And Attractive");
+
+// print number with concat
+console.log(9);
+console.log(10 + 8 );
+
+// if you have contact string with number or number with string .
+// but JS make a string 
+// like that
+console.log("I Love Bangladesh " + 10);
